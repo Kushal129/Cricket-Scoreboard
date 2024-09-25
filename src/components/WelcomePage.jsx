c85b2ea4d2a1d4b5b91a86e1147e26625131a441
kushal129 <21bmiit129@gmail.com>
2024-09-25 12:02:09 +0530

    init

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import bgimg from "../../public/bg.svg"
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import '../../src/index.css';
